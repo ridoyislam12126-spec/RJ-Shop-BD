6f442286680939f3020bb67056c712f7ff8281ca
Roman Ridoy <ridoyislam12126@gmail.com>
2026-02-26 10:53:35 +0600

    restore default page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,49 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main style={{ padding: "60px", textAlign: "center" }}>
-      <h1>Welcome to RJ Shop BD 🛒</h1>
-      <p>Website Updated Successfully 🚀</p>
-    </main>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
+
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            To get started, edit the page.tsx file.
+          </h1>
+
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Looking for a starting point or more instructions? Head over to
+            Templates or the Learning center.
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <a
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-black px-5 text-white transition-colors hover:bg-[#383838] md:w-[158px]"
+            href="https://vercel.com/new"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Deploy Now
+          </a>
+
+          <a
+            className="flex h-12 w-full items-center justify-center rounded-full border border-black/20 px-5 transition-colors hover:bg-black/5 md:w-[158px]"
+            href="https://nextjs.org/docs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Documentation
+          </a>
+        </div>
+      </main>
+    </div>
   );
 }
